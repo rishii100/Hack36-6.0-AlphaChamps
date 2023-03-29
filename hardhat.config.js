9826cb4ee0dcb87edf86b9e551bd897b0ce76502
Sameer Madhav D <sameermadhav.5@gmail.com>
2023-03-29 22:09:48 +0530

    third commit

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,6 +3,8 @@ require("dotenv").config();
 
 const POLYGON_MUMBAI_RPC = process.env.POLYGON_MUMBAI_RPC;
 const POLYGON_MUMBAI_KEY = process.env.POLYGON_MUMBAI_KEY;
+const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL;
+const GOERLI_KEY = process.env.GOERLI_KEY;
 
 module.exports = {
   solidity: "0.8.17",
@@ -21,7 +23,7 @@ module.exports = {
       chainId: 5,
       blockConfirmations: 6,
       url: GOERLI_RPC_URL,
-      accounts: [GOERLI_PK],
+      accounts: [GOERLI_KEY],
       gas: 50000000,
       maxFeePerGas: 250000000000,
       maxPriorityFeePerGas: 250000000000,
